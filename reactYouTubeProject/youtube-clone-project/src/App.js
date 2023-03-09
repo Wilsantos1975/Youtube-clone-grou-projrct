eb7bdb7c9963b64dd45b41feb3fb814f04321718
Wilsantos1975 <Faustosantos@pursuit.org>
2023-03-09 17:37:10 -0500

    more files added

--- reactYouTubeProject/youtube-clone-project/src/App.js
+++ reactYouTubeProject/youtube-clone-project/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import Search from './Components/Search';
 import VideoIndex from './Components/VideoIndex';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Search />
       <VideoIndex />
+      <Footer />
     </div>
   );
 }
